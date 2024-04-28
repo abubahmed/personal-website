f4a98b7f592ac343cea38d48092c1218e69617b7
adb8 <abuahmed0821@gmail.com>
2024-04-27 23:20:50 -0400

    sixth commit

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Social = ({ icon, name, link }) => {
   return (
     <a target="_blank" href={link}>
-      <img src={icon} alt={name} height={24} width={24} priority={true} className="md:mr-4 mx-2 md:ml-0" />
+      <img
+        src={icon}
+        alt={name}
+        height={24}
+        width={24}
+        priority={true}
+        className="md:mr-4 mx-2 md:ml-0"
+      />
     </a>
   );
 };
