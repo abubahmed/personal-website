7de5fc5eebdc0232928a8b5586d4e418a5c178ad
adb8 <abuahmed0821@gmail.com>
2024-05-11 15:41:34 -0400

    fifthteenth commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,13 +6,13 @@ const jetbrainsExtralight = JetBrains_Mono({ subsets: ["latin"], weight: "200" }
 const Navbar = () => {
   return (
     <ul className="flex max-w-[350px] justify-between mx-auto md:ml-auto md:mr-0 mt-6">
-      <li className={`${jetbrainsExtralight.className} cursor-pointer hover:underline text-md`}>
+      <li className={`${jetbrainsLight.className} cursor-pointer hover:underline text-md`}>
         <a href="#about">about</a>
       </li>
-      <li className={`${jetbrainsExtralight.className} cursor-pointer hover:underline text-md`}>
+      <li className={`${jetbrainsLight.className} cursor-pointer hover:underline text-md`}>
         <a href="#socials">socials</a>
       </li>
-      <li className={`${jetbrainsExtralight.className} cursor-pointer hover:underline text-md`}>
+      <li className={`${jetbrainsLight.className} cursor-pointer hover:underline text-md`}>
         <a href="#projects">projects</a>
       </li>
     </ul>
