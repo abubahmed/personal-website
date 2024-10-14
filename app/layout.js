3e9f32da981cecd3d5100bc4582f1ca2e645bb5a
adb8 <abuahmed0821@gmail.com>
2024-10-13 23:59:27 -0400

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import "./../styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
-  title: "Abu Ahmed",
-  description: "Abu Ahmed's personal website",
+  title: "Abu A",
+  description: "Abu A's personal website",
   keywords:
     "Abu Ahmed, Abu, Ahmed, personal, portfolio, github, linkedin, projects, socials, email, resume, website, web development, machine learning, data analysis, hackathons, Bronx High School of Science, Bronx Science, NYC, New York City, computer science, software engineering, student, high school, senior, programmer, coder, designer, engineer, developer, frontend, backend",
 };
@@ -11,14 +12,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PY7LNR0RZK"></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-PY7LNR0RZK');
-        </script>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.ico" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <GoogleAnalytics gaId="G-PY7LNR0RZK" />
+      </body>
     </html>
   );
 }
